b2587816986e73affc2c3f120db759f5e3d79a3d
Agnihotra Nath <agnihotranath@hotmail.com>
2024-11-09 20:22:19 +0530

    Uploading files.
    Setting up the basic presentation view.

---
+++ 182e9cc63c84fc52.en-IN.js
@@ -0,0 +1,2 @@
+window['cmsg']['assets'] = window['cmsg']['assets'] || {};
+window['cmsg']['assets']["en-IN"] = Object.assign(window['cmsg']['assets']["en-IN"] || {}, {"5706":{"js":["b8c953e36e15d5e7.strings.js"],"css":[]},"8701":{"js":["725787d8bc42d854.strings.js"],"css":[]}});
--- README.md
+++
@@ -1,2 +0,0 @@
-# alfaaarex.github.io
-Hostingwf export
